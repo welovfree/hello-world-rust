b4faa996b0031e512395e18dc748d391cad7926c
Mohamed Amziane <welovfree@gmail.com>
2025-06-26 08:11:49 +0100

    create hello and goodbye functions

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 /// A "Hello, world!" program.
-/// 
+///
 /// This is the best implementation of this program to ever exist.
 
 fn main() {
-    println!("Hello, world!");   
-     println!("Goodbye, world!");
+    print_hello();
+    print_goodbye();
+}
+
+fn print_hello() {
+    println!("Hello, world!");
+}
+
+fn print_goodbye() {
+    println!("Goodbye, world!");
 }
