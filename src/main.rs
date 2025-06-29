9b47f93043d72bac841d189d0445d87e37fbd209
Mohamed Amziane <welovfree@gmail.com>
2025-06-29 07:51:16 +0100

    add a comment to main

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 /// A "Hello, world!" program.
-/// 
+///
 /// This is the best implementation of this program to ever exist.
 
+/// add documentation for main
 fn main() {
     print("Hello, world!");
 }
